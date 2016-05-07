ab6271fd6e822fcf1253fb6cc9ad896a4bb43778
tsaikd <tsaikd@gmail.com>
2016-05-07 19:51:55 +0800

    update for KDGoLib
    Signed-off-by: tsaikd <tsaikd@gmail.com>

--- builder/logger.go
+++ builder/logger.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/codegangsta/cli"
 	"github.com/tsaikd/KDGoLib/errutil"
+	"github.com/tsaikd/KDGoLib/runtimecaller"
 )
 
+// RuntimeCallerFilterStopCliPackage filter CallInfo to stop after reach codegangsta/cli package
+func RuntimeCallerFilterStopCliPackage(callinfo runtimecaller.CallInfo) (valid bool, stop bool) {
+	if callinfo.PackageName == "github.com/codegangsta/cli" {
+		return false, true
+	}
+	return true, false
+}
+
 func init() {
-	errutil.AddRuntimeCallerFilter(func(packageName string, fileName string, funcName string, line int) bool {
-		switch packageName {
-		case "github.com/codegangsta/cli":
-			return false
-		}
-		return true
-	})
+	errutil.AddRuntimeCallerFilter(RuntimeCallerFilterStopCliPackage)
 }
 
 func actionWrapper(action func(context *cli.Context) error) func(context *cli.Context) {
